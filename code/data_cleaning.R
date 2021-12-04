769d030b70c299264635f37df7c268e0e3455399
Waveley Qiu <waveley.qiu@gmail.com>
2021-12-04 10:21:29 -0500

    updated data cleaning to use as.Date() instead of mdy()

--- code/data_cleaning.R
+++ code/data_cleaning.R
@@ -19,10 +19,14 @@ violation = read_csv("data/Open_Parking_and_Camera_Violations.csv") %>%
       borough %in% c("Q", "QN", "Qns") ~ "Queens",
       borough %in% c("ST", "R", "Rich", "RICH") ~ "Staten Island",
       borough %in% c("NY", "MN") ~ "Manhattan"),
-    issue_date = mdy(issue_date),
-    weekday = weekdays(issue_date),
+      issue_date = as.Date(issue_date, format = "%m/%d/%y"),
+      weekday = weekdays(issue_date),
+      year = year(issue_date),
+      month = month(issue_date),
+      day = day(issue_date)
   )  %>%  # make the borough the same 
   filter(borough != "A",                  # get rid of "A"
-         weekday != "NA") %>%   # remove data that cannot turn into weekday
-  separate(issue_date, into = c("year", "month", "day"), sep = "-")  # separate date into year, month, day
+         weekday != "NA") 
+  # %>%   # remove data that cannot turn into weekday
+  # separate(temp_issue_date, into = c("year", "month", "day"), sep = "-")  # separate date into year, month, day
 
