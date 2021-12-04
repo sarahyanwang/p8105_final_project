047cc8285110d8562c1c99326db100c26b7f9c1e
Waveley Qiu <waveley.qiu@gmail.com>
2021-12-03 21:38:57 -0500

    Waveley's commits. Updated file structure and added fines and Broadway analysis.

---
+++ code/data_cleaning.R
@@ -0,0 +1,28 @@
+
+### 
+# Data cleaning
+# Call by using source("code/data_cleaning.R") 
+###
+
+library(tidyverse)
+library(httr)
+library(lubridate)
+library(plotly)
+
+violation = read_csv("data/Open_Parking_and_Camera_Violations.csv") %>% 
+  janitor::clean_names() %>%
+  rename(borough = county) %>%  # rename county to borough
+  mutate(
+    borough = case_when(
+      borough %in% c("BK","K", "Kings") ~ "Brooklyn",
+      borough %in% c("BX", "Bronx") ~ "Bronx",
+      borough %in% c("Q", "QN", "Qns") ~ "Queens",
+      borough %in% c("ST", "R", "Rich", "RICH") ~ "Staten Island",
+      borough %in% c("NY", "MN") ~ "Manhattan"),
+    issue_date = mdy(issue_date),
+    weekday = weekdays(issue_date),
+  )  %>%  # make the borough the same 
+  filter(borough != "A",                  # get rid of "A"
+         weekday != "NA") %>%   # remove data that cannot turn into weekday
+  separate(issue_date, into = c("year", "month", "day"), sep = "-")  # separate date into year, month, day
+
