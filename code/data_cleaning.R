968fc654e037235ab648de29838cc497027619c4
Waveley Qiu <waveley.qiu@gmail.com>
2022-04-05 15:41:21 -0400

    post hoc commit

--- code/data_cleaning.R
+++ code/data_cleaning.R
@@ -1,33 +1,38 @@
 
 ### 
-# Data cleaning
+# Data Cleaning
 # Call by using source("code/data_cleaning.R") 
 ###
 
-library(jsonlite)
-library(progress)
 library(tidyverse)
 library(lubridate)
-library(plotly)
+
+##
+# basic data cleaning
+# 
+# - creates new borough variable and maps county codes to borough
+# - changes character issue_date to date variable and parses into month, year, date, and weekday
+# - removes month = 11 and 12 (not meaningful for this project)
+# - brings time (hour and minute) together and converts to 24 hour time
+##
 
 violation1 <-
-  read_csv("./Open_Parking_and_Camera_Violations.csv") %>% 
+  read_csv("../data/Open_Parking_and_Camera_Violations.csv") %>% 
   janitor::clean_names() %>%
-  rename(borough = county) %>%  # rename county to borough
   mutate(
     borough = case_when(
-      borough %in% c("BK","K", "Kings") ~ "Brooklyn",
-      borough %in% c("BX", "Bronx") ~ "Bronx",
-      borough %in% c("Q", "QN", "Qns") ~ "Queens",
-      borough %in% c("ST", "R", "Rich", "RICH") ~ "Staten Island",
-      borough %in% c("NY", "MN") ~ "Manhattan"),
+      county %in% c("BK","K", "Kings") ~ "Brooklyn",
+      county %in% c("BX", "Bronx") ~ "Bronx",
+      county %in% c("Q", "QN", "Qns") ~ "Queens",
+      county %in% c("ST", "R", "Rich", "RICH") ~ "Staten Island",
+      county %in% c("NY", "MN") ~ "Manhattan"),
       issue_date = as.Date(issue_date, format = "%m/%d/%Y"),
       weekday = weekdays(issue_date),
       year = year(issue_date),
       month = month(issue_date),
       day = day(issue_date)
-  )  %>%  # make the borough the same 
-  filter(borough != "A", 
+  )  %>% 
+  filter(county != "A", 
          weekday != "NA",
          month != "11",
          month != "12") %>%
@@ -35,8 +40,24 @@ violation1 <-
   mutate(hour = ifelse(substr(min, 3, 3) == "P", as.numeric(hour) + 12, hour)) %>%
   mutate(hour = as.numeric(hour))
 
+### 
+# geographical data cleaning
+#
+# - reads in second dataset and joins to first dataset
+# - creates house_street address:
+#   > if no street name, house_street is set to NA
+#   > if street name exists but house number does not, 0 is affixed to street name and output to house_street
+#   > if both elements are not NA, house_number affixed to street_name and output to hosue_street
+# - creates geo_nyc_address, to be fed into NYCGeoSearch API to return coordinate (sort of slow, free, not super precise):
+#   > concatenates the following if not NA: house_street, intersecting_street 
+#   > adds uppercase borough and "NEW YORK, NY"
+#   > separates text by ","
+# - creates google_address, to be fed into Google API to return coordinates (faster, not free, precise):
+#  > similar process to geo_nyc_address, but final element in the string is "New York, NY"
+###
+
 violation2 <- 
-  read_csv("./Parking_Violations_Issued_-_Fiscal_Year_2022.csv") %>%
+  read_csv("../data/Parking_Violations_Issued_-_Fiscal_Year_2022.csv") %>%
   janitor::clean_names() %>%
   rename(borough = violation_county) %>%
   select(house_number,
@@ -46,12 +67,12 @@ violation2 <-
          vehicle_body_type, 
          vehicle_year)
 
-
-
 violation <-
   violation1 %>% left_join(violation2, by = "summons_number") %>%
   mutate(
-    house_street = ifelse(is.na(street_name), NA, ifelse(is.na(house_number), paste("0", street_name), paste(house_number, street_name))),
+    house_street = ifelse(is.na(street_name), NA, 
+                          ifelse(is.na(house_number), paste("0", street_name), 
+                                 paste(house_number, street_name))),
     geo_nyc_address = paste(house_street[!is.na(house_street)], 
                             intersecting_street[!is.na(intersecting_street)], 
                             paste(toupper(borough), "COUNTY"),
@@ -65,6 +86,19 @@ violation <-
   ) %>%
   select(-house_number, -street_name, -intersecting_street)
 
+###
+# address_std: function to standardize commonly used address terms
+#
+# - changes address to uppercase
+# - maps the following:
+#   > PKY --> PARKWAY
+#   > BLVD, BL --> BOULEVARD
+#   > AVE, AV --> AVENUE
+#   > ST --> STREET
+#   > PL --> PLACE
+#   > HWY --> HIGHWAY
+#   > DR --> DRIVE
+###
 
 address_std <- function(ad){
   address <- 
@@ -81,19 +115,5 @@ address_std <- function(ad){
   return(address)
 }
 
+# applying address_std() to geo_nyc_address
 violation <- violation  %>% mutate(geo_nyc_address = geo_nyc_address %>% address_std())
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
